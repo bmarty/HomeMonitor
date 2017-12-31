a22bcf9dc99009d669e5e7f7a9efa3d3a63830b0
Benoit Marty <benoit.marty@niji.fr>
2017-12-31 09:40:06 +0100

    Move method

--- app/src/main/java/com/bmarty/homemonitor/ComUtils.kt
+++ app/src/main/java/com/bmarty/homemonitor/ComUtils.kt
@@ -1,9 +1,19 @@
 package com.bmarty.homemonitor
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.telephony.SmsManager
 import com.bmarty.homemonitor.data.Message
 import com.google.gson.Gson
+
+
+fun call(context: Context,
+         number: String = getDistantPhoneNumber(context)) {
+    val callIntent = Intent(Intent.ACTION_CALL)
+    callIntent.data = Uri.parse("tel:" + number)
+    context.startActivity(callIntent)
+}
 
 fun sendSms(context: Context,
             message: Message,
@@ -11,4 +21,3 @@ fun sendSms(context: Context,
     val smsManager: SmsManager = SmsManager.getDefault()
     smsManager.sendTextMessage(number, null, Gson().toJson(message), null, null)
 }
-
